849bac617b52caea5238222d31e522a5b026d1b8
Ben Haus <ben@little-haus.com>
2020-05-08 11:25:59 -0400

    index and server set up

---
+++ projectRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const projectDB = require('./data/helpers/projectModel');
+const actionDB = require('./data/helpers/actionModel');
+
+
+
+
+
+module.exports = router;
